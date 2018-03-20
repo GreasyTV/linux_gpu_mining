cbbe54000e970556e9c82380e2cd4a90fc249bc8
GreasyTV <37587665+GreasyTV@users.noreply.github.com>
2018-03-20 16:07:22 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-./miner --server  us.hushmine.pro --user t1VoR8JXjCPY2iSwFYdA8zVrrgKEvxeJEXH.createthis/YOUR_EMAIL --pec --fee 0 --pass z --port 9009
+./miner --server  eu1.ethermine.org:4444 --user 0x5956275181db8364b963bf6daaefdf6a4caf82df.createthis/YOUR_EMAIL --pec --fee 0 --pass z --port 9009
